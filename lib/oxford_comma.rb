de7171895053966b08dfa44b95ee140b1a4a20ac
Sherry <sherryptk@gmail.com>
2017-11-06 16:52:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
 
+  if array.size == 3
+    array.insert(1,", ")
+    array.insert(3,", and ")
+    array.join
+  elsif array.size>3
+    last = array.pop
+    array.pop!
+
+    array.each do |fruit|
+      fruit + ", "
+    end
+
+    array << "and " + last
+    array.join
+   else array.join(" and ")
+   end
+
+
 end
