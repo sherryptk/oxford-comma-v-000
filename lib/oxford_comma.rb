67eadecd11ddb2d514e8e8ff52a4ecc183c81736
Sherry <sherryptk@gmail.com>
2017-11-06 17:24:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
 
+  if array.size > 2
+    last = array.pop
+    array.each do |fruit|
+      fruit << (", ")
+    end
+    array << "and " + last
+    array.join
+  else array.join(" and ")
+  end
+
 end
