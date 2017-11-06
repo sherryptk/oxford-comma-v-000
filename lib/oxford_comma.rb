f7844a7d1c2d9c105e4ba414d8ce01615380f888
Sherry <sherryptk@gmail.com>
2017-11-06 17:04:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.size == 3
     array.insert(1,", ")
     array.insert(3,", and ")
-    array.join
+    # array.join
   elsif array.size>3
     last = array.pop
 
@@ -12,10 +12,8 @@ def oxford_comma(array)
     end
 
     array << "and " + last
-    array.join
-
+    # array.join
    else array.join(" and ")
    end
-
 
 end
